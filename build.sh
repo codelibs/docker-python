be77712bd567ad27fabe33fba1bbf8c403383dc1
Shinsuke Sugaya <shinsuke@apache.org>
2021-07-06 08:12:36 +0900

    add use_buildx

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ PYTHON_VERSION=3.9
 PYTHON_OS_CODE=buster
 DOCKER_FILE_URL=https://raw.githubusercontent.com/docker-library/python/master/${PYTHON_VERSION}/${PYTHON_OS_CODE}/Dockerfile
 BUILD_OS_CODE=bionic
+USE_BUILDX=false
 
 echo "Downloading ${DOCKER_FILE_URL}"
 curl ${DOCKER_FILE_URL} \
@@ -15,5 +16,9 @@ PYTHON_VERSION=`cat Dockerfile | grep "ENV PYTHON_VERSION" | awk '{ print $3 }'`
 DOCKER_TAG=ghcr.io/codelibs/python:${PYTHON_VERSION}-${BUILD_OS_CODE}
 
 echo "Building ${DOCKER_TAG}"
-docker build --rm -t ${DOCKER_TAG} .
+if [[ ${USE_BUILDX} != "true" ]] ; then
+  docker build --rm -t ${DOCKER_TAG} .
+else
+  docker buildx build --rm --platform linux/amd64,linux/arm64 -t ${DOCKER_TAG} --push .
+fi
 
